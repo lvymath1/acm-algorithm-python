b8e81651d5a548b3f5e4869640c38acc477bed41
lvyang <lvyang@tom.com>
2024-06-04 21:45:43 +0800

    first

--- code-template/sort/bubble_sort.py
+++ code-template/sort/bubble_sort.py
@@ -1,6 +1,8 @@
 from typing import List
 
-
+# 在0~i范围上，相邻较大值数滚下去，最大值来到i，然后在0~i-1的范围上继续
+# 时间复杂度: O(n^2)
+# 空间复杂度：O(1)
 def bubble_sort(arr: List[int]):
     n = len(arr)
     for i in range(n - 1, -1, -1):
