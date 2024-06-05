c83389d1648c79b7d9200b90af1ad8f45c764c9d
lvyang <lvyang@tom.com>
2024-06-05 17:03:38 +0800

    first

--- code-template/binary_search/binary_search.py
+++ code-template/binary_search/binary_search.py
@@ -1,4 +1,5 @@
 import bisect
+from typing import List
 
 
 # 找 >= 最左位置, 不存在返回列表长度
@@ -8,3 +9,18 @@ def bisect_ge_left(arr, x):
 # 找 <= 最右位置，不存在返回-1
 def bisect_le_right(arr, x):
     return bisect.bisect_right(arr, x) - 1
+
+"""
+二分答案:
+[162. 寻找峰值](https://leetcode.cn/problems/find-peak-element/description/)
+"""
+def findPeakElement(nums: List[int]) -> int:
+    n = len(nums)
+    left, right = -1, n
+    while left + 1 < right:
+        mid = (left + right) // 2
+        if (mid == 0 or nums[mid - 1] < nums[mid]):
+            left = mid
+        else:
+            right = mid
+    return left
